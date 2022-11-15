af32940847e17d9c199b531d2f9afc9b1ae35c88
Edinichkin <11th@bk.ru>
2022-11-15 22:10:26 +0300

    Homework is done

--- src/main/java/com/eleventh/employees/service/EmployeeService.java
+++ src/main/java/com/eleventh/employees/service/EmployeeService.java
@@ -4,9 +4,8 @@ import com.eleventh.employees.model.Employee;
 import com.eleventh.employees.record.EmployeeRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
@@ -16,8 +15,45 @@ public class EmployeeService {
         return employees.values();
     }
 
+    public int getSalarySum() {
+        return employees.entrySet().stream()
+                .mapToInt(e -> e.getValue().getSalary())
+                .sum();
+    }
+
+    public List<Employee> getEmployeesWithHighSalary() {
+        int averageSalary = getSalaryAverage();
+        return employees.values().stream()
+                .filter(e -> e.getSalary() > averageSalary)
+                .collect(Collectors.toList());
+    }
+
+    public int getSalaryAverage(){
+        int averageSalary = 0;
+        if (employees.values().size() > 0) {
+            averageSalary = employees.values().stream()
+                    .mapToInt(Employee::getSalary)
+                    .sum() / employees.values().size();
+        }
+        return averageSalary;
+    }
+
+    public Employee getEmployeeWithMaxSalary() {
+        return employees.entrySet().stream()
+                .map(e -> e.getValue())
+                .max(Comparator.comparing(e -> e.getSalary()))
+                .get();
+    }
+
+    public Employee getEmployeeWithMinSalary() {
+        return employees.entrySet().stream()
+                .map(e -> e.getValue())
+                .min(Comparator.comparing(e -> e.getSalary()))
+                .get();
+    }
+
     public Employee addEmployee(EmployeeRequest employeeRequest) {
-        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null){
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
             throw new IllegalArgumentException("Employee name should not be empty");
         }
         Employee employee = new Employee(
